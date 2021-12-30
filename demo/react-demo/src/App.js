65cd0858188d0207c185a249faa21da05ff1e26d
2333Ge <ych.class@qq.com>
2021-12-30 19:35:21 +0800

    atuo commit 2021-12-30 19:35:21

--- demo/react-demo/src/App.js
+++ demo/react-demo/src/App.js
@@ -1,13 +1,17 @@
 import ContextType from "./demo/context/ConsumerDemo";
 import UIOptimization from "./demo/uioptimization";
 import ManyData from "./demo/manyData";
+import APIDemo from "./demo/react-api";
+import Ref from "./demo/ref";
 
 function App() {
   return (
     <div>
-      <ContextType />
-      <UIOptimization />
+      {/* <ContextType />
+      <UIOptimization /> */}
+      {/* <APIDemo /> */}
       {/* <ManyData /> */}
+      <Ref />
     </div>
   );
 }
