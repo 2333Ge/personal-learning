78b7add109369c1c3f38523ce6489d190b948ea8
2333Ge <ych.class@qq.com>
2022-07-12 23:30:59 +0800

    deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,13 +10,21 @@ npm run build
 # 进入生成的文件夹
 cd docs
 
-# git init
-# git add -A
-# git commit -m 'deploy'
-# git remote add origin https://gitee.com/i2333g3/personal-learning-blog.git
-# git push -u -f origin master
+git init
+git add -A
+git commit -m 'deploy'
+git remote add origin https://gitee.com/i2333g3/personal-learning-blog.git
+git push -u -f origin master
 
 cd -
 
-echo "$(date +"%Y-%m-%d %H:%M:%S")" >> my_actions
+# github actions 触发部署
+cd my_actions
 
+echo "publish: $(date +"%Y-%m-%d %H:%M:%S")" > publish.md
+git add .
+git commit -m "publish"
+
+git push -u -f origin main
+
+cd -
